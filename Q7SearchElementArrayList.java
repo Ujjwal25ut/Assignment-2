e1cc55ba7ee8b1bf4309e2a2cbd1102e0c2d22d6
Ujjwal Tyagi <108051454+Ujjwal25ut@users.noreply.github.com>
2022-07-01 14:15:22 +0530

    Update Q7SearchElementArrayList.java

--- Q7SearchElementArrayList.java
+++ Q7SearchElementArrayList.java
@@ -3,14 +3,13 @@ package assignment2June24;
 	import java.util.*;
 	  public class Q7SearchElementArrayList {
 	  public static void main(String[] args) {
-	  // Creae a list and add some colors to the list
 	  List<String> list_Strings = new ArrayList<String>();
 	  list_Strings.add("Red");
 	  list_Strings.add("Green");
 	  list_Strings.add("Orange");
 	  list_Strings.add("White");
 	  list_Strings.add("Black");
-	  // Search the value Red
+	 
 	    if (list_Strings.contains("Reds")) {
 	    System.out.println("Found the element");
 	    } else {
